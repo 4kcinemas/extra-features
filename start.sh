9b4fc54f2989d5fe701487d1d113b3c813a93cc8
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-03 22:08:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/4kcinemas/Naruto.git /Naruto
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /Naruto
 fi
-cd /Ajax
+cd /Naruto
 pip3 install -U -r requirements.txt
-echo "Starting ᗩᒍᗩ᙭....🔥"
+echo "Starting 𝙽𝚊𝚛𝚞𝚝𝚘....🔥"
 python3 bot.py
