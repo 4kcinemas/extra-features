51668b94cc35ad58d48b666d8c60f8346e383b2b
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-05 15:10:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -69,10 +69,10 @@ async def start(client, message):
         ]
 
         if message.command[1] != "subscribe":
-            btn.append([InlineKeyboardButton("❔ T R Y  N O W ❔", callback_data=f"checksub#{message.command[1]}")])
+            btn.append([InlineKeyboardButton("❔ T R Y  N O W ❔", callback_data=f"checksub#{message.command[1]}"])
         await client.send_message(
             chat_id=message.from_user.id,
-            text="**𝑱𝒐𝒊𝒏 𝑶𝒖𝒓 𝑴𝒐𝒗𝒊𝒆 𝑼𝒑𝒅𝒂𝒕𝒆𝒔 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 𝑻𝒐 𝑼𝒔𝒆 𝑻𝒉𝒊𝒔 𝑩𝒐𝒕!**",
+            text="**Please Join Our Channel to Get Movies!**",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode="markdown"
             )
