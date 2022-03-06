c16b221b392974c4c1e649c7e1074933c6e7e223
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-06 18:38:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1313,7 +1313,7 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
-        k = await msg.reply("I couldn't find anything related to that. Check your spelling")
+        k = await msg.reply_str("CAACAgUAAxkBAAEN125iJLH_huw6lTleKqOxmqwCxLGpiAACugIAAkGL-VTQ6rE0nuUb2B4E")
         await asyncio.sleep(8)
         await k.delete()
         return
