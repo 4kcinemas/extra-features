2fd34abb626abfe735d546f3855eab1f5e4a80cb
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-09 18:48:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -47,7 +47,7 @@ async def start(client, message):
             InlineKeyboardButton('❗ Help ❗', callback_data='help'),
             ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_text(),
+        await message.reply_text(
             text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
@@ -85,8 +85,7 @@ async def start(client, message):
             InlineKeyboardButton('❗ Help ❗', callback_data='help'),
             ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
+        await message.reply_text(
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
