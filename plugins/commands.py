3aa8ecb304e41a1f710b34595f070447219cc33d
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-09 22:07:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -46,7 +46,7 @@ async def start(client, message):
             ],[[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''), 
             InlineKeyboardButton('Help💭', callback_data='help'),
-            ]]]
+            ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_text(
             text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
