c2457622713ee1c461f6d206af3f2798bb2977ba
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-14 02:29:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -108,7 +108,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"『📂{get_size(file.file_size)}』 {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"『📥{get_size(file.file_size)}』 {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -1209,7 +1209,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"『📂{get_size(file.file_size)}』 {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"『📥{get_size(file.file_size)}』 {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
