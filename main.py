bf9bbe5a04ee6d683a67d138fd0ddaf17ec30b7e
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-19 20:49:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app=client(
     api_hash="2903597c04eb4a6d799ff8ffc87d4aac",
     bot_token="5222423549:AAFpbj40J4lgKgdbGbUl76HqUvFgtScA9DE"
 )
+
+
+@app.on_message(filters.command("about"))
+async def about(bot, message):
+    await message.reply_text(
