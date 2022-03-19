4ce8284d66b3006fd5ec02c19ca2144a5a8e9de8
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-19 21:05:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from pyrogram import Client, filters
 
-App=Client(
+Client(
     "Netflix Bot",
     api_id="11762352",
     api_hash="2903597c04eb4a6d799ff8ffc87d4aac",
     bot_token="5222423549:AAFpbj40J4lgKgdbGbUl76HqUvFgtScA9DE"
 )
 
-@App.on_message(filters.command("about"))
+@Client.on_message(filters.command("about"))
 async def about(bot, message):
     await message.reply(script.ABOUT_TXT)
 
 
 
-App.run()
+Client.run()
