6c90096131f6a3496cff22f42b51ff293ee3a9cc
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-19 20:41:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from pyrogram import client
 
 app=client(
-    
+    "Netflix Bot",
+    api_id="11762352",
+    api_hash="2903597c04eb4a6d799ff8ffc87d4aac",
+    bot_token="5222423549:AAFpbj40J4lgKgdbGbUl76HqUvFgtScA9DE"
+)
