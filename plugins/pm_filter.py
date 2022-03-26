8187da91d97f32573c5ca194d8ce060ee18d9606
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-26 13:48:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -85,7 +85,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer(⚠️ 𝗛𝗲𝘆! 𝗧𝗵𝗶𝘀 𝗶𝘀 𝗡𝗼𝘁 𝗙𝗼𝗿 𝗬𝗼𝘂 𝗣𝗹𝗲𝗮𝘀𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗬𝗼𝘂𝗿 𝗢𝘄𝗻.", show_alert=True)
+        return await query.answer("⚠️ 𝗛𝗲𝘆! 𝗧𝗵𝗶𝘀 𝗶𝘀 𝗡𝗼𝘁 𝗙𝗼𝗿 𝗬𝗼𝘂 𝗣𝗹𝗲𝗮𝘀𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗬𝗼𝘂𝗿 𝗢𝘄𝗻.", show_alert=True)
     try:
         offset = int(offset)
     except:
